c2dafb6694c191e31001f1a305c30af8b4d09185
2011WEB <159939647+2011WEB@users.noreply.github.com>
2024-02-14 22:11:39 +0330

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,43 +1,13 @@
-document.querySelector('.loginForm').addEventListener('submit', (e) => {
-    e.preventDefault();
+function validate(){
+    var password = document.getElementById("pass").value;
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    if (validateUsername(username) && validatePassword(password)) {
-        if (username === 'Mohannad' && password === 'Mo20012001') {
-            window.open('https://forms.gle/6WapG8Af7pRaXTsQA');
-        } else {
-            alert('Invalid username or password');
-        }
-    } else {
-        alert('Invalid username or password');
-    }
-});
-
-function validateUsername(username) {
-    if (username.length < 3) {
+    if(password === "12345678"){
+        alert("Login Succesfull");
+        window.location.replace("newpage.html");
         return false;
     }
-    return true;
-}
-
-function validatePassword(password) {
-    const length = password.length;
-    if (length < 8) {
-        return false;
+    else{
+        alert("Login Failed");
+        return false; // Prevent form submission
     }
-    if (!/[A-Z]/.test(password)) {
-        return false;
-    }
-    if (!/[a-z]/.test(password)) {
-        return false;
-    }
-    if (!/[0-9]/.test(password)) {
-        return false;
-    }
-    if (!/[!@#$%^&*]/.test(password)) {
-        return false;
-    }
-    return true;
 }
