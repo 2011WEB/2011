1908a18e2059f9e73c281e896e978960592e0614
2011WEB <159939647+2011WEB@users.noreply.github.com>
2024-02-14 22:13:58 +0330

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,9 +1,9 @@
 function validate(){
     var password = document.getElementById("pass").value;
 
-    if(password === "12345678"){
+    if(password === "19784264546063618705062008295248056747018545255478"){
         alert("Login Succesfull");
-        window.location.replace("newpage.html");
+        window.location.replace("https://forms.gle/6WapG8Af7pRaXTsQA");
         return false;
     }
     else{
