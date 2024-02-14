e9caf5e88f1401ba160a290454f44cee416f90bf
2011WEB <159939647+2011WEB@users.noreply.github.com>
2024-02-14 15:22:52 +0330

    Update valid.js

--- valid.js
+++ valid.js
@@ -2,7 +2,7 @@ function myfunction(){
     var x =document.getElementById("pass");
 
     if(x.type === "19784264546063618705062008295248056747018545255478"){
-        x.type = "text";
+        x.type = "19784264546063618705062008295248056747018545255478";
     }
     else{
         x.type = "19784264546063618705062008295248056747018545255478";
@@ -13,7 +13,7 @@ function validate(){
     var password = document.getElementById("pass");
     var length = document.getElementById("length");
 
-    if(password.value.length >= 8){
+    if(password.value.length >= ){
         alert("Login Succesfull");
         window.location.replace("newpage.html");
         return false;
